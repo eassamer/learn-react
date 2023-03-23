ead488a3fb3f2e8f13a0ac0ff12b2bd3d33b2b72
Elmehdi Assamer <eassamer@e1r4p4.1337.ma>
2023-03-23 21:08:48 +0000

    react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Button } from '@nextui-org/react';
 import Logo from './logo/Logo';
 import Google from './logo/Google';
 import { css } from '@emotion/react'
+import Back from './logo/Back';
 import wave from './wave.svg'
+import Rightimg from './rightImg.svg';
 // import logo1 from './42.png';
 const App = () => {
     const ButtonStyleIntra = css `
@@ -13,7 +15,7 @@ const App = () => {
     font-weight: 600;
     font-size: 16px;
     border-radius: 10px;
-    margin-top: 40px;
+    margin-top: 30px;
     `
     const ButtonStyleGoogle = css `
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@600;700&display=swap');
@@ -22,9 +24,13 @@ const App = () => {
     font-weight: 600;
     font-size: 16px;
     border-radius: 10px;
-    margin-top: 15px;
     `
-
+    const SmallButton = css `
+        width: 150px;
+        background-color: #A19E99;
+        border-radius: 12px;
+        padding: 12px;
+    `
     return (
         <div className="app">
             <div className='login-page'>
@@ -47,8 +53,16 @@ const App = () => {
                     <div className='buttons'>
                         <Button className='button-i' iconRight={<Google />}size="xl" css={ButtonStyleGoogle}>Google</Button>
                     </div>
+                    <div className='buttons'>
+                        <Button iconRight={<Back/>} auto css={SmallButton}></Button>
+                    </div>
+                    <div className='small-text'>
+                        <p>Go Back</p>
+                    </div>
                 </div>
-                <div className='right'></div>
+                <div className='right'>
+                    <img src={Rightimg} alt="right-pic"/>
+                </div>
             </div>
         </div>
     );
